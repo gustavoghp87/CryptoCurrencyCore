356a882ed3f5b67c60079fcadb7e045d71a0c113
Gustavo Hernán Perrupato <ghp.2120@gmail.com>
2021-06-25 18:57:27 -0300

    human hash score

--- Services/HashScore.cs
+++ Services/HashScore.cs
@@ -3,7 +3,7 @@ using System.Numerics;
 
 namespace cryptoCurrency.Services
 {
-    public static class ScoreHash
+    public static class HashScore
     {
         private static int hashLength = 64;
         private static int maxScore = 35;
@@ -13,6 +13,13 @@ namespace cryptoCurrency.Services
             "f" , "g" , "h" , "i" , "j" , "k" , "l" , "m" , "n" , "o" , "p" , "q" , "r" ,
             "s" , "t" , "u" , "v" , "w" , "x" , "y" , "z"
         };
+        public static string Get(string hash)
+        {
+            BigInteger score = Score(hash);
+            int zeros = GetZeros(hash);
+            BigInteger rest = GetRest(score, zeros);
+            return zeros.ToString() + " zeros + " + rest.ToString();
+        }
         public static BigInteger Score(string hash)
         {
             if (hash.Length != hashLength) return 0;
@@ -32,6 +39,24 @@ namespace cryptoCurrency.Services
                 if (c == _characters[i]) return maxScore - i;
             }
             return 0;
+        }
+        private static int GetZeros(string hash)
+        {
+            int zeros = 0;
+            foreach (char digit in hash.ToCharArray())
+            {
+                if (digit.ToString() == "0") zeros += 1;
+                else return zeros;
+            }
+            return zeros;
+        }
+        private static BigInteger GetRest(BigInteger score, int zeros)
+        {
+            for (int i = 0; i < zeros; i++)
+            {
+                score -= maxScore * (BigInteger)Math.Pow(maxScore+1, hashLength-1-i);
+            }
+            return score;
         }
     }
 }
